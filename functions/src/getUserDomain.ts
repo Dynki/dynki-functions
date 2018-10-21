78b1fa0868e30ed82eb6a6fce03340f665bf817a
Dean Selvey <deanselvey@gmail.com>
2018-10-21 19:56:03 +0100

    Added meat to get user domain function

--- functions/src/getUserDomain.ts
+++ functions/src/getUserDomain.ts
@@ -2,5 +2,5 @@ import * as functions from 'firebase-functions';
 import { firestore } from 'firebase-admin';
 
 export const getUserDomain = functions.https.onRequest((request, response) => {
-    firestore().collection('domains').get()
+    response.write(firestore().collection('user-domains').doc(request.body.uid));
 });
