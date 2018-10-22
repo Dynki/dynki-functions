54548d46ffee09cb177c42fd367baf4e1066ff2b
Dean Selvey <deanselvey@gmail.com>
2018-10-22 20:36:55 +0100

    get user domain was using write not send for response

--- functions/src/domain.ts
+++ functions/src/domain.ts
@@ -2,7 +2,7 @@ import * as functions from 'firebase-functions';
 import { firestore } from 'firebase-admin';
 
 export const getUserDomain = functions.https.onRequest((request, response) => {
-    response.write(firestore().collection('user-domains').doc(request.body.uid));
+    response.send(firestore().collection('user-domains').doc(request.body.uid));
 });
 
 export const createUserDomain = functions.https.onRequest((request, response) => {
